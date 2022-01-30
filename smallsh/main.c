e3fa1493d68c745e1ecd54605af0ef9584e92896
jry2112 <jry2112@columbia.edu>
2022-01-30 18:53:40 -0500

    Add project files.

---
+++ smallsh/main.c
@@ -0,0 +1,25 @@
+/*
+* Author: Jada Young
+* Date: 1/30/2022
+* Date Last Modified: 2/7/2022
+* CS344: Assignment 3 - smallsh
+* Requirements: Write a program that: 1) Provide a prompt for running commands;
+*    2) Handle blank lines and comments, which are lines beginning with the # character;
+*    3) Provide expansion for the variable $$;
+*    4) Execute 3 commands exit, cd, and status via code built into the shell;
+*    5) Execute other commands by creating new processes using a function from; the exec family of functions
+*    6) Support input and output redirection;
+*    7) Support running commands in foreground and background processes;
+*    8) Implement custom handlers for 2 signals, SIGINT and SIGTSTP;
+*/
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include "smallsh.h"
+
+int main(int argc, char* argvp[])
+{
+	printf("Hello World");
+
+	return 0;
+}
---
+++ smallsh/smallsh.h
@@ -0,0 +1,8 @@
+#ifndef SMALLSH_H_   /* Include guard */
+#define SMALLSH_H_
+
+void getCommand(int argc, char* argvp[]);
+
+
+
+#endif
